e1c872abf28aca98063966f3122262d6a350d96b
Hassan Saad <41341768+HassanSaad0X@users.noreply.github.com>
2020-04-02 14:14:05 +0200

    Update EmailsFinder.py

--- EmailsFinder.py
+++ EmailsFinder.py
@@ -1,23 +1,26 @@
 from pyhunter import PyHunter
 import sys
 
-if len(sys.argv) != 3:
-	print "Usage:   ", sys.argv[0], "Domain APIkey"
-	print "Example: ", sys.argv[0], "microsoft.com 0123456789"
+if len(sys.argv) != 5:
+	print "Usage:   ", sys.argv[0], "Domain Count Start APIkey"
+	print "Example: ", sys.argv[0], "microsoft.com 20 10 0123456789"
+	print "Note: Hunter API Maximum Count is 100"
 	exit()
 
 domain = sys.argv[1]
-API = sys.argv[2]
+count = int(sys.argv[2])
+start = int(sys.argv[3])
+API = sys.argv[4]
 
-print "Domain: ", domain
-print "API Key: ", API
+print "Domain:", domain
+print "Count:", count
+print "Start:", start
+print "API Key:", API
 print "#############################################"
 
 hunter = PyHunter(API)
 
-for num in range(0,5):
-	data = hunter.domain_search(company=domain, limit=100, offset=num*100)
+data = hunter.domain_search(company=domain, limit=count, offset=start)
 
-	for x in data["emails"]:
-		print x["value"]
-
+for x in data["emails"]:
+	print x["value"]
